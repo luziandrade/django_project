ec092b8f01d923b2f8c5b842d8d46e1baadd041d
Luana Andrade <luzi015@hotmail.com>
2019-12-09 22:43:25 +0000

    hostname

--- django_project/settings.py
+++ django_project/settings.py
@@ -29,12 +29,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = [os.environ.get('HOSTNAME')]
 
-host = os.environ.get("SITE_HOST")
-if host:
-    ALLOWED_HOSTS.append(host)
-
-
-#django-list.herokuapp.com
 # Application definition
 
 INSTALLED_APPS = [
@@ -126,10 +120,4 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
